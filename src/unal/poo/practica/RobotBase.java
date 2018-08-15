09dd56a4aac271f813f7894f065df99f83ce0cf0
Estudiante <Estudiante@10.203.134.135>
2018-08-15 09:01:19 -0500

    Comit Inicial

--- src/unal/poo/practica/RobotBase.java
+++ src/unal/poo/practica/RobotBase.java
@@ -19,41 +19,77 @@ public class RobotBase
             
             //Direction.NORTH, EAST, SOUTH, WEST
             //Definicion de la ubicacion del robot, Ciudad, posicion, Direccion, Numero things en el bolso.
-            estudiante = new Robot(objetos,0, 2, Direction.EAST,10);
+            estudiante = new Robot(objetos,0, 1, Direction.SOUTH,10);
             
-	    //Mover una interseccion en el sentido al cual este apuntando el objeto.
-            estudiante.move ();
-            
-            //Girar a la izquierda
+            /*Field 1
+            estudiante = new Robot(objetos,0, 2, Direction.WETS,10);
             estudiante.turnLeft();
+            estudiante.turnLeft();
+            estudiante.move();
+            estudiante.turnLeft();
+            estudiante.turnLeft();
+            estudiante.turnLeft();
+            estudiante.move();
+            estudiante.move();
+            estudiante.move();
+            estudiante.turnLeft();
+            estudiante.turnLeft();
+            estudiante.turnLeft();
+            estudiante.move();
+            estudiante.move();
+            estudiante.move();
+            estudiante.turnLeft();
+            estudiante.turnLeft();
+            estudiante.turnLeft();
+            estudiante.move();
+            estudiante.move();
+            estudiante.move();
+            estudiante.turnLeft();
+            estudiante.turnLeft();
+            estudiante.turnLeft();
+            estudiante.move();
+            estudiante.move();
+            estudiante.turnLeft();
+            estudiante.turnLeft();
+            */
             
-            //Tomando decisiones, Si puedo tomar un Thing
-            boolean puedeTomar = estudiante.canPickThing();
+            /*Field 2
+            estudiante = new Robot(objetos,1, 2, Direction.SOUTH,10);
+            for(int i=0;i<=2;i++){
+                estudiante.turnLeft();
+            }
+            for(int i=0;i<=2;i++){
+                estudiante.move();
+                estudiante.turnLeft();
+            }
+            estudiante.turnLeft();
+            for(int i=0;i<=2;i++){
+                estudiante.move();
+                for(int j=0;j<=2;j++){
+                    estudiante.turnLeft();
+                }
+            }*/
             
-            //Tomar un Thing
-            if(puedeTomar == true)
-               estudiante.pickThing();
+            //Field 3
+            estudiante.move();
+            if(estudiante.canPickThing()==true){
+                estudiante.pickThing();
+            }
+             for(int j=1;j<=3;j++){
+                 estudiante.turnLeft();
+            }
+            estudiante.countThingsInBackpack();
+            int n = estudiante.countThingsInBackpack();
+            for(int j=1;j<=n;j++){
+                estudiante.putThing();
+                estudiante.move();
+                }
+            }
             
-            //Especifica el numero de Thing que tiene en robot en el bolso
-            int numeroThings = estudiante.countThingsInBackpack();
-            
-            //Poner Thing, se debe validar que tenga things en el bolso
-            estudiante.putThing();
-                       
-            //Si el frente esta libre de Wall
-            estudiante.frontIsClear();
-            
-            //Invocando una funcion
-            creacionFuncion(4);
-            
-            //Toman un Thing
-            estudiante.pickThing();
-            
-            
-	}
+	
         
         public static void creacionFuncion(int parametroEntrada){
-            for (int i = 0; i < parametroEntrada; i++) 
+            for (int i = 0; i < parametroEntrada; i++)
                 estudiante.move();
         }
 }
